1385df10b4fbd815429a98ee7b52c8e87923a3d6
Keita <keitaw09@gmail.com>
2019-09-17 07:48:05 +0900

    Longest substring without repeating characters.

--- Longest_Substring_Without_Repeating_Characters.py
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -1,39 +1,28 @@
 class Solution:
 
     def lengthOfLongestSubstring(self, string: str) -> int:
-        """Function that returns the LSWRC.
-
-        Longest Substring Without Repeating Characters (LSWRC)
-        """
-        # Function that returns the longest substring
-        #
-        NO_OF_CHARS = 256
-        n = len(string)
+        if len(string) == 0: return 0
+        NCHARS = 256
         # Initialize the visited array as -1, -1 is used to indicate
         # that character has not been visited yet.
-        visited = [-1] * NO_OF_CHARS
-        # Mark the first character as visited by storing the index of
-        # first character in visited array.
-        visited[ord(string[0])] = 0
-        # Start from the second character. First character is already
-        # processed (cur_len and max_len are initialized as 1, and 
-        # visited[ord(string[0])] is set 
-        cur_len = 1     # To store the length of current string
-        max_len = 1     # To store the longest substring
-        prev_index = 0  # To store the previous index
-        for i in range(1, n):
+        # Non-zero value corresponds to the index that the character appeared.
+        visited = [-1] * NCHARS
+        n = len(string)
+        curr_len = 0  # To store the length of current string
+        max_len = 1   # To store the longest substring
+        for i in range(n):
+            # prev_index here indicates the last index that the character observed.
             prev_index = visited[ord(string[i])]
-
-            # If the current character is not present in the already
-            # processed substirng or it is not part of the current
-            # longest substring, then do cur_len++
-            if prev_index == -1 or (i - cur_len > prev_index):
-                cur_len += 1
+            if (prev_index == -1) or (i - curr_len > prev_index):
+                curr_len += 1
             else:
-                if cur_len > max_len:
-                    max_len = cur_len
-                cur_len = i - prev_index
+                curr_len = i - prev_index
+            if curr_len > max_len:
+                max_len = curr_len
             visited[ord(string[i])] = i
-        if cur_len > max_len:
-            max_len = cur_len
         return max_len
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.lengthOfLongestSubstring("pwwkew"))
+    print(solution.lengthOfLongestSubstring("aab"))
