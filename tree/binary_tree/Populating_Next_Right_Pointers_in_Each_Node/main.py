a1e79e8b6efa73ed5fff7eb00b1e72e3693ccb76
Keita <keitaw09@gmail.com>
2019-09-22 10:10:53 +0900

    Add a default value.

--- tree/binary_tree/Populating_Next_Right_Pointers_in_Each_Node/main.py
+++ tree/binary_tree/Populating_Next_Right_Pointers_in_Each_Node/main.py
@@ -4,7 +4,7 @@
 
 
 class Node:
-    def __init__(self, val, left, right, next):
+    def __init__(self, val, left, right, next=None):
         self.val = val
         self.left = left
         self.right = right
