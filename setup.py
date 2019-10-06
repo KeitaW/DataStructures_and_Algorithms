418d5b2d6d1a27ab991837dd40e14221a2d767c4
Keita Watanabe <keitaw09@gmail.com>
2019-10-06 10:23:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,8 @@ from setuptools import find_packages
 setup(
     name="leetcode",
     version="develop",
-    description="Log of leet codes.",
+    description="Log of programming exercises",
     author="Keita Watanabe",
     maintainer_email="keitaw09@gmail.com",
-    # install_requires=[
-    #    "dataclasses~=0.6",
-    #    "ruamel.yaml~=0.15.99",
-    #    "numpy~=1.16.4",
-    #    "scipy~=1.2.1",
-    #    "torch~=1.2.0",
-    # ],
     packages=find_packages(),
 )
