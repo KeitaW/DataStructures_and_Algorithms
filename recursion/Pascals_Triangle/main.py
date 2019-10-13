2488091083ee7628a6886c7e969729e041eff9fa
Keita Watanabe <keitaw09@gmail.com>
2019-10-14 07:36:34 +0900

    Add description of the problem

--- recursion/Pascals_Triangle/main.py
+++ recursion/Pascals_Triangle/main.py
@@ -3,7 +3,22 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        """Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
 
+        Parameters
+        ----------
+        numRows : int
+            The row index
+
+        Returns
+        -------
+        List[List[int]]
+            The numRows th index row of the Pascal's triangle
+
+        Notes
+        ------
+        You can only use O(k) extra space.
+        """
         def init(row, triangle=[]):
             return init(row-1, [[1]*row] + triangle) if row > 0 else triangle
         triangle = init(numRows)
