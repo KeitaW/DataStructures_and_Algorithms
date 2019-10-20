f1d2d95209b7254f88f2a77bd4c2ff7cf491153c
Keita Watanabe <keitaw09@gmail.com>
2019-10-21 08:03:25 +0900

    Update

--- recursion/Sort_an_Array/main.py
+++ recursion/Sort_an_Array/main.py
@@ -1,3 +1,34 @@
+from typing import List
+
+
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        pass
+        return self.merge_sort(nums)
+
+    def sort(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        return nums
+
+    def merge(self, left, right):
+        """Merge two sorted arrays
+        """
+        left_cursor = right_cursor = 0
+        merged = []
+        while left_cursor < len(left) and right_cursor < len(right):
+            if left[left_cursor] < right[right_cursor]:
+                merged.append(left[left_cursor])
+                left_cursor += 1
+            else:
+                merged.append(right[right_cursor])
+                right_cursor += 1
+        merged += left[left_cursor:]
+        merged += right[right_cursor:]
+        return merged
+
+    def merge_sort(self, nums: List[int]) -> List[int]:
+        if len(nums) == 1:
+            return nums
+        pivot = len(nums) // 2
+        nums[:pivot] = self.merge_sort(nums[:pivot])
+        nums[pivot:] = self.merge_sort(nums[pivot:])
+        return self.merge(nums[:pivot], nums[pivot:])
--- recursion/Sort_an_Array/test_main.py
+++ recursion/Sort_an_Array/test_main.py
@@ -1,1 +1,8 @@
 from recursion.Sort_an_Array.main import Solution
+
+
+def test_main():
+    solution = Solution()
+    # test merge
+    assert solution.merge([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    assert solution.sortArray([1, 5, 3]) == [1, 3, 5]
