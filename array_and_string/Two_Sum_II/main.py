99ca816253b39ed04ece59b80da1232bb0b657c7
Keita Watanabe <keitaw09@gmail.com>
2019-10-30 08:24:18 +0900

    Too slow

---
+++ array_and_string/Two_Sum_II/main.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)):
+            for j in range(i+1, len(numbers)):
+                if numbers[i] + numbers[j] == target:
+                    return sorted([i+1, j+1])
---
+++ array_and_string/Two_Sum_II/test_main.py
@@ -0,0 +1,6 @@
+from array_and_string.Two_Sum_II.main import Solution
+
+
+def test_main():
+    solution = Solution()
+    solution.twoSum([2, 7, 11, 15], 9) == [1, 2]
