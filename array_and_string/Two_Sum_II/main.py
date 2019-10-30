4fe9c0564f5df6a9d84dbb6bd1d8631db796c35e
Keita Watanabe <keitaw09@gmail.com>
2019-10-31 07:56:07 +0900

    Update

--- array_and_string/Two_Sum_II/main.py
+++ array_and_string/Two_Sum_II/main.py
@@ -3,6 +3,30 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """[summary]
+        Assumption: "numbers" is sorted
+
+        Parameters
+        ----------
+        numbers : List[int]
+        target : int
+
+        Returns
+        -------
+        List[int]
+             Two indices that 
+        """
+        small, large = 0, len(numbers) - 1
+        while True:
+            summed = numbers[small] + numbers[large]
+            if summed == target:
+                return small+1, large+1
+            if summed > target:
+                large -= 1
+            else:
+                small += 1
+
+    def naive_twoSum(self, numbers: List[int], target: int) -> List[int]:
         for i in range(len(numbers)):
             for j in range(i+1, len(numbers)):
                 if numbers[i] + numbers[j] == target:
