011e5f2422a93cd61aa8355eeefd252dc69d12e6
Keita Watanabe <keitaw09@gmail.com>
2019-11-06 07:16:03 +0900

    Update

--- binary_search/Binary_Search/main.py
+++ binary_search/Binary_Search/main.py
@@ -2,7 +2,19 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search_iterI(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return - 1
+
+    def search_recurI(self, nums: List[int], target: int) -> int:
         def binary_search(left, right):
             if left > right:
                 return -1
