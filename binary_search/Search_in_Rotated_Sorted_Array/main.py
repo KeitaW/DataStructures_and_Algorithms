58dc925c9beb98f6a183ba68ee605acbfec6c9ae
Keita Watanabe <keitaw09@gmail.com>
2019-11-07 09:20:08 +0900

    Still WIP

--- binary_search/Search_in_Rotated_Sorted_Array/main.py
+++ binary_search/Search_in_Rotated_Sorted_Array/main.py
@@ -1,4 +1,5 @@
 class Solution(object):
+
     def search(self, nums, target):
         """
         :type nums: List[int]
@@ -6,3 +7,20 @@ class Solution(object):
         :rtype: int
         """
         pass
+
+
+def find_rotation_index(nums):
+    if nums[0] <= nums[-1]:
+        return 0
+
+    def binary_search(left, right):
+        pivot = (left + right) // 2
+        if nums[pivot + 1] < nums[pivot]:
+            return pivot + 1
+        if nums[left] < nums[pivot]:
+            # [left, pivot] is sorted =>
+            # rotation index should be in the right half
+            return binary_search(pivot + 1, right)
+        else:
+            return binary_search(left, pivot - 1)
+    return binary_search(0, len(nums) - 1)
--- binary_search/Search_in_Rotated_Sorted_Array/test_main.py
+++ binary_search/Search_in_Rotated_Sorted_Array/test_main.py
@@ -1,4 +1,10 @@
-from binary_search.Search_in_Rotated_Sorted_Array.main import Solution
+from binary_search.Search_in_Rotated_Sorted_Array.main import find_rotation_index, Solution
+
+
+def test_find_rotation_index():
+    assert find_rotation_index([1]) == 0
+    assert find_rotation_index([4, 5, 6, 1, 2, 3]) == 3
+    assert find_rotation_index([3, 1, 2]) == 1
 
 
 def test_main():
