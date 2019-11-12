08aa8e6611ce46b88ac2cd7e815e0ee2688785ed
Keita Watanabe <keitaw09@gmail.com>
2019-11-12 16:50:44 +0900

    Status in aero

---
+++ linkedlist/DesignLinkedList/main.py
@@ -0,0 +1,65 @@
+
+class Node:
+    def __init__(self, val=-1):
+        self.val: int = val
+        self.next_node: Node = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        if self.head is None:
+            self.head = Node(val)
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        node = self.head
+        while node.next_node:
+            node = node.next_node
+        node.next_node = Node(val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        node = self.head
+        for _ in range(index-1):
+            if node.next_node is None:
+                return
+            node = node.next_node
+        next_next_node = node.next_node
+        node.next_node = Node(val)
+        node.next_node.next_next_node = next_next_node
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        for _ in range(index-1):
+            if node.next_node is None:
+                return
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
