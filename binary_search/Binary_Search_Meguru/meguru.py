a17ad79022281812ba16e26d95b094a1b09c28a7
Keita Watanabe <keitaw09@gmail.com>
2019-11-17 10:43:57 +0900

    Update

--- binary_search/Binary_Search_Meguru/meguru.py
+++ binary_search/Binary_Search_Meguru/meguru.py
@@ -5,7 +5,34 @@ https://qiita.com/drken/items/97e37dd6143e33a64c8c
 from typing import List, Callable, Union
 
 
-def binary_search_meguru(f: Callable[[int], bool], left: int, right: int) -> int:
+# def binary_search_meguru(f: Callable[[int], bool], ok: int, ng: int) -> int:
+#    """General purpose binary search algorithm
+#
+#    Parameters
+#    ----------
+#    f : Callable[[int], int]
+#        Monotonically increasing function
+#    left : int
+#        The left initial value that satisfies f(x) == True
+#    right : int
+#        The right initial value that satisfies f(x) == False
+#
+#    Returns
+#    -------
+#    int
+#        The minimum x that satisfies f(x) = True
+#    """
+#    def search(ok, ng):
+#        mid = (ok + ng) // 2
+#        return (
+#            ok if abs(ok - ng) <= 1 else
+#            search(mid, ng) if f(mid) else
+#            search(ok, mid)
+#        )
+#    return search(ok, ng)
+
+
+def binary_search_meguru(f: Callable[[int], bool], ok: int, ng: int) -> int:
     """General purpose binary search algorithm
 
     Parameters
@@ -22,41 +49,14 @@ def binary_search_meguru(f: Callable[[int], bool], left: int, right: int) -> int
     int
         The minimum x that satisfies f(x) = True
     """
-    def search(left, right):
-        mid = (left + right) // 2
-        return (
-            right if right - left <= 1 else
-            search(left, mid) if f(mid) else
-            search(mid, right)
-        )
-    return search(left, right)
-
-
-def binary_search_meguru_iter(f: Callable[[int], bool], left: int, right: int) -> int:
-    """General purpose binary search algorithm
-
-    Parameters
-    ----------
-    f : Callable[[int], int]
-        Monotonically increasing function
-    left : int
-        The left initial value that satisfies f(x) == True
-    right : int
-        The right initial value that satisfies f(x) == False
-
-    Returns
-    -------
-    int
-        The minimum x that satisfies f(x) = True
-    """
-    while right - left > 1:
-        print(left, right)
-        mid = (left + right) // 2
+    while abs(ok - ng) > 1:
+        print(ok, ng)
+        mid = (ok + ng) // 2
         if f(mid):
-            right = mid
+            ok = mid
         else:
-            left = mid
-    return right
+            ng = mid
+    return ok
 
 
 def lower_bound(arr: List[int], target: int) -> int:
@@ -74,8 +74,8 @@ def lower_bound(arr: List[int], target: int) -> int:
         index of the minimum element. -1 if all the elements are less than target
     """
     # なぜこれで良いか考える
-    left, right = -1, len(arr)
-    return binary_search_meguru(lambda x: arr[x] >= target, left, right)
+    ng, ok = -1, len(arr)
+    return binary_search_meguru(lambda x: arr[x] >= target, ok, ng)
 
 
 def upper_bound(arr: List[int], target: int) -> int:
@@ -93,8 +93,18 @@ def upper_bound(arr: List[int], target: int) -> int:
         index of the minimum element. -1 if all the elements are less than target
     """
     # なぜこれで良いか考える
-    left, right = -1, len(arr)
-    return binary_search_meguru(lambda x: arr[x] >= target, left, right)
+    ng, ok = len(arr), -1
+    return binary_search_meguru(lambda x: arr[x] <= target, ok, ng)
+
+
+def find_leftmost(arr: List[int], target: int) -> int:
+    lb = lower_bound(arr, target)
+    return lb if arr[lb] == target else -1
+
+
+def find_rightmost(arr: List[int], target: int) -> int:
+    ub = upper_bound(arr, target)
+    return ub if arr[ub] == target else -1
 
 
 if __name__ == "__main__":
@@ -104,9 +114,18 @@ if __name__ == "__main__":
     print(f"arr: {arr}, target: {2}, lower bound: {lower_bound(arr, 2)}")
     arr = [2, 2, 2]
     print(f"arr: {arr}, target: {2}, lower bound: {lower_bound(arr, 2)}")
+    arr = [2, 2, 2]
+    print(f"arr: {arr}, target: {1}, lower bound: {lower_bound(arr, 1)}")
     arr = [1, 2, 3, 4, 5]
     print(f"arr: {arr}, target: {2}, upper bound: {upper_bound(arr, 2)}")
     arr = [1, 1, 2, 2, 3, 4, 5]
     print(f"arr: {arr}, target: {2}, upper bound: {upper_bound(arr, 2)}")
     arr = [2, 2, 2]
     print(f"arr: {arr}, target: {2}, upper bound: {upper_bound(arr, 2)}")
+    arr = [2, 2, 2]
+    print(f"arr: {arr}, target: {1}, upper bound: {upper_bound(arr, 1)}")
+
+    arr = [1, 1, 2, 2, 3, 4, 5]
+    print(f"arr: {arr}, target: {2}, find leftmost: {find_leftmost(arr, 2)}")
+    arr = [1, 1, 2, 2, 3, 4, 5]
+    print(f"arr: {arr}, target: {2}, find rightmost: {find_rightmost(arr, 2)}")
