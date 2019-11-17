cb5cf0d89dfc3c915ac326366671a1d618d0ab8b
Keita Watanabe <keitaw09@gmail.com>
2019-11-17 10:46:27 +0900

    Clean up codes

--- binary_search/Binary_Search_Meguru/meguru.py
+++ binary_search/Binary_Search_Meguru/meguru.py
@@ -5,31 +5,31 @@ https://qiita.com/drken/items/97e37dd6143e33a64c8c
 from typing import List, Callable, Union
 
 
-# def binary_search_meguru(f: Callable[[int], bool], ok: int, ng: int) -> int:
-#    """General purpose binary search algorithm
-#
-#    Parameters
-#    ----------
-#    f : Callable[[int], int]
-#        Monotonically increasing function
-#    left : int
-#        The left initial value that satisfies f(x) == True
-#    right : int
-#        The right initial value that satisfies f(x) == False
-#
-#    Returns
-#    -------
-#    int
-#        The minimum x that satisfies f(x) = True
-#    """
-#    def search(ok, ng):
-#        mid = (ok + ng) // 2
-#        return (
-#            ok if abs(ok - ng) <= 1 else
-#            search(mid, ng) if f(mid) else
-#            search(ok, mid)
-#        )
-#    return search(ok, ng)
+def binary_search_meguru_recur(f: Callable[[int], bool], ok: int, ng: int) -> int:
+    """General purpose binary search algorithm
+
+    Parameters
+    ----------
+    f : Callable[[int], int]
+        Monotonically increasing function
+    left : int
+        The left initial value that satisfies f(x) == True
+    right : int
+        The right initial value that satisfies f(x) == False
+
+    Returns
+    -------
+    int
+        The minimum x that satisfies f(x) = True
+    """
+    def search(ok, ng):
+        mid = (ok + ng) // 2
+        return (
+            ok if abs(ok - ng) <= 1 else
+            search(mid, ng) if f(mid) else
+            search(ok, mid)
+        )
+    return search(ok, ng)
 
 
 def binary_search_meguru(f: Callable[[int], bool], ok: int, ng: int) -> int:
@@ -50,7 +50,6 @@ def binary_search_meguru(f: Callable[[int], bool], ok: int, ng: int) -> int:
         The minimum x that satisfies f(x) = True
     """
     while abs(ok - ng) > 1:
-        print(ok, ng)
         mid = (ok + ng) // 2
         if f(mid):
             ok = mid
@@ -73,7 +72,6 @@ def lower_bound(arr: List[int], target: int) -> int:
     int
         index of the minimum element. -1 if all the elements are less than target
     """
-    # なぜこれで良いか考える
     ng, ok = -1, len(arr)
     return binary_search_meguru(lambda x: arr[x] >= target, ok, ng)
 
@@ -92,7 +90,6 @@ def upper_bound(arr: List[int], target: int) -> int:
     int
         index of the minimum element. -1 if all the elements are less than target
     """
-    # なぜこれで良いか考える
     ng, ok = len(arr), -1
     return binary_search_meguru(lambda x: arr[x] <= target, ok, ng)
 
