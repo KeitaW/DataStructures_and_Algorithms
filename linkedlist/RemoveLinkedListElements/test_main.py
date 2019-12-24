339771ce96c798cbe130678c87a38f7e5808b8bf
Keita Watanabe <keitaw09@gmail.com>
2019-12-25 08:55:23 +0900

    Test cases

---
+++ linkedlist/RemoveLinkedListElements/test_main.py
@@ -0,0 +1,21 @@
+from linkedlist.RemoveLinkedListElements.main import ListNode, Solution
+
+
+def test_main():
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    solution = Solution()
+    solution.removeElements(3)
+    assert head.val == 1
+    assert head.next.val == 2
+    assert head.next.next.val == 4
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(2)
+    head.next.next.next = ListNode(4)
+    solution = Solution()
+    solution.removeElements(2)
+    assert head.val == 1
+    assert head.next.val == 4
