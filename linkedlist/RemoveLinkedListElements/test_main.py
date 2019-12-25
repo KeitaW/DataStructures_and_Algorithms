2f79b8fd6771648ad2989f937b7200087f24717b
Keita Watanabe <keitaw09@gmail.com>
2019-12-25 09:12:03 +0900

    Add a test case

--- linkedlist/RemoveLinkedListElements/test_main.py
+++ linkedlist/RemoveLinkedListElements/test_main.py
@@ -19,3 +19,12 @@ def test_main():
     solution.removeElements(head, 2)
     assert head.val == 1
     assert head.next.val == 4
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    solution = Solution()
+    solution.removeElements(head, 1)
+    assert head.val == 2
+    assert head.next.val == 3
+    assert head.next.next.val == 4
