be0c2315eafcb8cda59bd833daa1e5b8246635f6
Keita Watanabe <keitaw09@gmail.com>
2019-12-25 10:28:31 +0900

    Accepted

--- linkedlist/RemoveLinkedListElements/main.py
+++ linkedlist/RemoveLinkedListElements/main.py
@@ -7,6 +7,25 @@ class ListNode:
     def __str__(self, string=""):
         return string + f"{self.val}, " + (self.next.__str__() if self.next else "")
 
+
 class Solution:
+    def __init__(self):
+        self.head = None
+
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        pass
+        if head is None:
+            return head
+        self.head = head
+        while (self.head is not None) and (self.head.val == val):
+            self.head = self.head.next
+        if (self.head is None) or (self.head.next is None):
+            return self.head
+        prev, curr = self.head, self.head.next
+        while curr is not None:
+            if curr.val == val:
+                prev.next = prev.next.next
+                curr = curr.next
+            else:
+                prev = prev.next
+                curr = curr.next
+        return self.head
--- linkedlist/RemoveLinkedListElements/test_main.py
+++ linkedlist/RemoveLinkedListElements/test_main.py
@@ -7,24 +7,29 @@ def test_main():
     head.next.next = ListNode(3)
     head.next.next.next = ListNode(4)
     solution = Solution()
-    solution.removeElements(head, 3)
-    assert head.val == 1
-    assert head.next.val == 2
-    assert head.next.next.val == 4
+    ans = solution.removeElements(head, 3)
+    assert ans.val == 1
+    assert ans.next.val == 2
+    assert ans.next.next.val == 4
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = ListNode(2)
     head.next.next.next = ListNode(4)
     solution = Solution()
-    solution.removeElements(head, 2)
-    assert head.val == 1
-    assert head.next.val == 4
+    ans = solution.removeElements(head, 2)
+    assert ans.val == 1
+    assert ans.next.val == 4
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = ListNode(3)
     head.next.next.next = ListNode(4)
     solution = Solution()
-    solution.removeElements(head, 1)
-    assert head.val == 2
-    assert head.next.val == 3
-    assert head.next.next.val == 4
+    ans = solution.removeElements(head, 1)
+    assert ans.val == 2
+    assert ans.next.val == 3
+    assert ans.next.next.val == 4
+    head = ListNode(1)
+    head.next = ListNode(1)
+    solution = Solution()
+    ans = solution.removeElements(head, 1)
+    assert ans is None
