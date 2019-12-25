fac3fd23dac12a589dc4df7c5f27a0cf670662d1
Keita Watanabe <keitaw09@gmail.com>
2019-12-25 09:21:37 +0900

    Add str for debbuging

--- linkedlist/RemoveLinkedListElements/main.py
+++ linkedlist/RemoveLinkedListElements/main.py
@@ -4,6 +4,8 @@ class ListNode:
         self.val = x
         self.next = None
 
+    def __str__(self, string=""):
+        return string + f"{self.val}, " + (self.next.__str__() if self.next else "")
 
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
