346431c4a7a166dfd047345a002407b98d6d374c
Keita Watanabe <keitaw09@gmail.com>
2019-12-27 11:57:45 +0900

    Add debug function

--- linkedlist/PalindromeLinkedList/main.py
+++ linkedlist/PalindromeLinkedList/main.py
@@ -2,7 +2,9 @@
 class ListNode:
     def __init__(self, x):
         self.val = x
-        self.next = None
+
+    def __str__(self, string=""):
+        return string + f"{self.val}, " + (self.next.__str__() if self.next else "")
 
 
 def find_middle(head: ListNode):
