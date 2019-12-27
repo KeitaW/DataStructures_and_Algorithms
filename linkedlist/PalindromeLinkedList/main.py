dfcf7dd258ca89750ff8bc2285214b324aece232
Keita Watanabe <keitaw09@gmail.com>
2019-12-27 12:07:23 +0900

    Fix

--- linkedlist/PalindromeLinkedList/main.py
+++ linkedlist/PalindromeLinkedList/main.py
@@ -2,6 +2,7 @@
 class ListNode:
     def __init__(self, x):
         self.val = x
+        self.next = None
 
     def __str__(self, string=""):
         return string + f"{self.val}, " + (self.next.__str__() if self.next else "")
@@ -13,6 +14,25 @@ def find_middle(head: ListNode):
         slow = slow.next
         fast = fast.next.next
     return slow
+
+
+def compare_list(head1: ListNode, head2: ListNode):
+    while (head1 is not None) and (head2 is not None):
+        if head1.val != head2.val:
+            return False
+        head1 = head1.next
+        head2 = head2.next
+    return True
+
+
+def reverse_list(head: ListNode):
+    pre, peri, post = None, head, head.next
+    while peri is not None:
+        post = peri.next
+        peri.next = pre
+        pre = peri
+        peri = post
+    head = pre
 
 
 class Solution:
--- linkedlist/PalindromeLinkedList/test_main.py
+++ linkedlist/PalindromeLinkedList/test_main.py
@@ -1,4 +1,4 @@
-from linkedlist.PalindromeLinkedList.main import ListNode, Solution, find_middle
+from linkedlist.PalindromeLinkedList.main import ListNode, Solution, find_middle, compare_list, reverse_list
 
 
 def test_find_middle():
@@ -16,6 +16,33 @@ def test_find_middle():
     head.next.next = ListNode(3)
     head.next.next.next = ListNode(4)
     assert find_middle(head).val == 3
+
+
+def test_reverse_list():
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    reverse_list(head)
+    assert head.val == 3
+    assert head.next.val == 2
+    assert head.next.nextval == 1
+
+
+def test_compare_list():
+    head1 = ListNode(1)
+    head1.next = ListNode(2)
+    head1.next.next = ListNode(3)
+    head2 = ListNode(1)
+    head2.next = ListNode(2)
+    head2.next.next = ListNode(3)
+    assert compare_list(head1, head2)
+    head1 = ListNode(1)
+    head1.next = ListNode(2)
+    head1.next.next = ListNode(3)
+    head2 = ListNode(1)
+    head2.next = ListNode(3)
+    assert not compare_list(head1, head2)
 
 
 def test_main():
