0f63ffad629dae9aabffbe2ec45b842e6d2b26e6
Keita Watanabe <keitaw09@gmail.com>
2019-12-27 10:22:22 +0900

    Add find middle function

--- linkedlist/PalindromeLinkedList/main.py
+++ linkedlist/PalindromeLinkedList/main.py
@@ -5,6 +5,28 @@ class ListNode:
         self.next = None
 
 
+def find_middle(head: ListNode):
+    slow = fast = head
+    while (fast is not None) and (fast.next is not None):
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        pass
+        """ Given a singly linked list, determine if it is a palindrome.
+        Input: 1->2->2->1
+        Output: true
+
+        Parameters
+        ----------
+        head : ListNode
+
+        Returns
+        -------
+        bool
+        """
+        tail = head
+        while tail.next is not None:
+            tail = tail.next
--- linkedlist/PalindromeLinkedList/test_main.py
+++ linkedlist/PalindromeLinkedList/test_main.py
@@ -1,7 +1,25 @@
-from linkedlist.PalindromeLinkedList.main import ListNode, Solution
+from linkedlist.PalindromeLinkedList.main import ListNode, Solution, find_middle
+
+
+def test_find_middle():
+    head = ListNode(1)
+    assert find_middle(head).val == 1
+    head = ListNode(1)
+    head.next = ListNode(2)
+    assert find_middle(head).val == 2
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    assert find_middle(head).val == 2
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    assert find_middle(head).val == 3
 
 
 def test_main():
+    return
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = ListNode(2)
