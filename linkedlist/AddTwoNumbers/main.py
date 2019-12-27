e6034d39b36a1855a0c2df3754daceddf86fcbaf
Keita Watanabe <keitaw09@gmail.com>
2019-12-27 17:16:17 +0900

    Add docstring

--- linkedlist/AddTwoNumbers/main.py
+++ linkedlist/AddTwoNumbers/main.py
@@ -10,6 +10,22 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        Example:
+        Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        Output: 7 -> 0 -> 8
+        Explanation: 342 + 465 = 807.
+
+        Parameters
+        ----------
+        l1 : ListNode
+        l2 : ListNode
+
+        Returns
+        -------
+        ListNode
+        """
         head = node = ListNode(-1)  # Head is dummy
         carry = 0
         while (l1 is not None) or (l2 is not None):
