3142699f56d3d0d53e4a69197a864ec14bd2a4ef
Keita Watanabe <keitaw09@gmail.com>
2019-12-28 16:47:04 +0900

    WIP

--- queue_and_stack/NumberOfIslands/main.py
+++ queue_and_stack/NumberOfIslands/main.py
@@ -1,8 +1,19 @@
 from typing import List
 from queue import Queue
+from itertools import product
+
+LAND = "1"
+WATER = "0"
 
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        pass
+        if len(grid) == 0:
+            return 0
+        nrow, ncol = len(grid), len(grid[0])
+        num_islands = 0
+        neighbors = Queue()
+        for row, col in product(range(nrow), range(ncol)):
+            if grid[row][col] == LAND:
+                pass
 
