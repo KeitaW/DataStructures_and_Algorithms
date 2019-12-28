8ec8f11e1731127827d2545f1ef5c88b3abd5a23
Keita Watanabe <keitaw09@gmail.com>
2019-12-28 18:54:18 +0900

    First version

--- queue_and_stack/OpenTheLock/main.py
+++ queue_and_stack/OpenTheLock/main.py
@@ -1,3 +1,9 @@
+from typing import Generator
+from queue import Queue
+
+NDIGITS = 4
+
+
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         """You have a lock in front of you with 4 circular wheels.
@@ -19,5 +25,28 @@ class Solution:
         -------
         int
         """
-        pass
+
+        def neighbors(digits: str) -> Generator[str, None, None]:
+            for i in range(NDIGITS):
+                digit = int(digits[i])
+                for diff in [1, -1]:
+                    next_digit = (digit + diff) % 10
+                    yield digits[:i] + str(next_digit) + digits[i:]
+
+        deadends = set(deadends)
+        digits = "0000"
+        visited = {digits}
+        queue = Queue()
+        queue.put((digits, 0))  # The second element represents number of operations.
+        while not queue.empty():
+            digits, n_operations = queue.get()
+            if digits == target:
+                return n_operations
+            if digits in deadends:
+                continue
+            for next_digits in neighbors(digits):
+                if next_digits not in visited:
+                    visited.add(next_digits)
+                    queue.put((next_digits, n_operations + 1))
+        return -1
 
