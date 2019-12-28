2cfded582a5323b4cf634c05b45c7c923d8a6991
Keita Watanabe <keitaw09@gmail.com>
2019-12-28 13:17:11 +0900

    Add skelton

--- queue_and_stack/WallsAndGates/test_main.py
+++ queue_and_stack/WallsAndGates/test_main.py
@@ -1,7 +1,15 @@
 from queue_and_stack.WallsAndGates.main import Solution
 
+INF = 2 ** 31 - 1
+
 
 def test_main():
     solution = Solution()
-    rooms = [[]]
+    rooms = [
+        [INF, -1, 0, INF],
+        [INF, INF, INF, -1],
+        [INF, -1, INF, -1],
+        [0, -1, INF, INF],
+    ]
     solution.wallsAndGates(rooms)
+    assert rooms == [[3, -1, 0, 1], [2, 2, 1, -1], [1, -1, 2, -1], [0, -1, 3, 4]]
