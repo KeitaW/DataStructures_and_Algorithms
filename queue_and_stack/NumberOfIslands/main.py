0bfbfccfdcfd796586f609ec18879b0b1bc7520f
Keita Watanabe <keitaw09@gmail.com>
2019-12-28 18:05:59 +0900

    The first version

--- queue_and_stack/NumberOfIslands/main.py
+++ queue_and_stack/NumberOfIslands/main.py
@@ -13,7 +13,21 @@ class Solution:
         nrow, ncol = len(grid), len(grid[0])
         num_islands = 0
         neighbors = Queue()
-        for row, col in product(range(nrow), range(ncol)):
-            if grid[row][col] == LAND:
-                pass
+        for r, c in product(range(nrow), range(ncol)):
+            if grid[r][c] == LAND:
+                num_islands += 1
+                neighbors.put((r, c))
+            while not neighbors.empty():
+                row, col = neighbors.get()
+                if row - 1 >= 0 and grid[row - 1][col] == LAND:
+                    neighbors.put((row - 1, col))
+                    grid[row - 1][col] = WATER
+                if row + 1 <= nrow - 1 and grid[row + 1][col] == LAND:
+                    neighbors.put((row + 1, col))
+                    grid[row + 1][col] = WATER
+                if col - 1 >= 0 and grid[row][col - 1] == LAND:
+                    neighbors.put((row, col - 1))
+                if col + 1 <= ncol - 1 and grid[row][col + 1] == LAND:
+                    neighbors.put((row, col + 1))
+        return num_islands
 
