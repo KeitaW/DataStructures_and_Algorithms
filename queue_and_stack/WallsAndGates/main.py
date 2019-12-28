25dc46b97e1e2407021e3a2c692e10d9ed35c35f
Keita Watanabe <keitaw09@gmail.com>
2019-12-28 13:32:18 +0900

    WIP

--- queue_and_stack/WallsAndGates/main.py
+++ queue_and_stack/WallsAndGates/main.py
@@ -1,4 +1,11 @@
 from typing import List
+from queue import Queue
+
+WALL = -1
+GATE = 0
+EMPTY = 2 ** 31 - 1
+
+DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
 class Solution:
@@ -14,4 +21,19 @@ class Solution:
         if len(rooms) == 0:
             return rooms
         height, width = len(rooms), len(rooms[0])
+        queue = Queue()
+        # Enqueue all gates
+        for row in range(height):
+            for col in range(width):
+                if rooms[row][col] == GATE:
+                    queue.put((row, col))
+        while not queue.empty():
+            row, col = queue.get()
+            for direction in DIRECTIONS:
+                pass
 
+            if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) {
+                continue;
+            }
+            rooms[r][c] = rooms[row][col] + 1;
+            q.add(new int[] { r, c });
