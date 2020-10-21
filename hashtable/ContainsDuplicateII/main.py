76c2f28ee9ac034cfa9183bddb03fd0e5b36a6f8
Keita Watanabe <keitaw09@gmail.com>
2020-10-21 20:16:23 +0900

    update

--- hashtable/ContainsDuplicateII/main.py
+++ hashtable/ContainsDuplicateII/main.py
@@ -3,9 +3,9 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        for idx in range(len(nums) - k):
-            for l in range(k - 1):
-                if nums[idx] == nums[idx + l]:
+        for idx in range(len(nums)):
+            for l in range(max(idx - k, 0), idx):
+                if nums[idx] == nums[l]:
                     return True
         else:
             return False
--- hashtable/ContainsDuplicateII/test_main.py
+++ hashtable/ContainsDuplicateII/test_main.py
@@ -3,4 +3,6 @@ from hashtable.ContainsDuplicateII.main import Solution
 
 def test_main():
     solution = Solution()
-    assert solution.containsNearbyDuplicate([1, 2, 3, 1], 3) == True
+    assert solution.containsNearbyDuplicate([1, 2, 3, 1], 3) is True
+    assert solution.containsNearbyDuplicate([1, 0, 1, 1], 1) is True
+    assert solution.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2) is False
