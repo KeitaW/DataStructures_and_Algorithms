a8fc53fd5775d0a13f143749e395260700522dfe
Keita Watanabe <keitaw09@gmail.com>
2020-10-21 20:50:45 +0900

    add another solution

--- hashtable/ContainsDuplicateII/main.py
+++ hashtable/ContainsDuplicateII/main.py
@@ -3,10 +3,19 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window = set()
+        for idx in range(len(nums)):
+            if nums[idx] in window:
+                return True
+            window.add(nums[idx])
+            if len(window) > k:
+                window.remove(nums[idx - k])
+        return False
+
+    def containsNearbyDuplicate_naive(self, nums: List[int], k: int) -> bool:
         for idx in range(len(nums)):
             for l in range(max(idx - k, 0), idx):
                 if nums[idx] == nums[l]:
                     return True
         else:
             return False
-
