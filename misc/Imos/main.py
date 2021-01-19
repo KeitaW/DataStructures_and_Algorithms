023f9f1d6c19a06a6dd821a738507c7fe7f36ef9
Keita Watanabe <keitaw09@gmail.com>
2021-01-19 09:15:53 +0900

    add problem discription

--- misc/Imos/main.py
+++ misc/Imos/main.py
@@ -38,6 +38,12 @@ class Imos1d:
         return max(table)
 
 
+"""問題
+あなたは様々な種類のモンスターを捕まえるゲームをしています．今あなたがいるのは W×H のタイルからなる草むらです．この草むらでは N 種類のモンスターが現れます．モンスター i は Ai≦x<Bi, Ci≦y<Di の範囲にしか現れません．このゲームを効率的に進めるため，1 つのタイル上で現れるモンスターの種類の最大値が知りたいです．（ただし，W×H は計算可能な程度の大きさとし，N は大きくなりうるものとします．
+https://imoz.jp/algorithms/imos_method.html
+"""
+
+
 class Imos2d:
     def __init__(self) -> None:
         pass
